ae80ad628115c76d5b5623cb16c7a3b680d4a459
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-28 02:35:39 +0000

    done

---
+++ ICS3U-Unit4-07.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Emmanuel
+# Created on: Nov 2022
+# This program shows every number from 1000 to 2000
+
+
+def main():
+
+
+    # Process and Output
+    for counter in range(1000, 2001):
+        if counter % 5 == 0:
+            print("")
+            print(counter, end=" ")
+        else:
+            print("{0} ".format(counter), end="")
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
